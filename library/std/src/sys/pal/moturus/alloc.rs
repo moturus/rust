284e3f778b1e28d4cb953278cf8afced474dddd9
U. Lasiotus <lasiotus@proton.me>
2024-09-02 11:15:02 -0700

    Switch alloc to moto-rt.

--- library/std/src/sys/pal/moturus/alloc.rs
+++ library/std/src/sys/pal/moturus/alloc.rs
@@ -4,21 +4,21 @@ use crate::alloc::{GlobalAlloc, Layout, System};
 unsafe impl GlobalAlloc for System {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        moto_runtime::alloc(layout)
+        moto_rt::alloc(layout)
     }
 
     #[inline]
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        moto_runtime::alloc_zeroed(layout)
+        moto_rt::alloc_zeroed(layout)
     }
 
     #[inline]
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        moto_runtime::dealloc(ptr, layout)
+        moto_rt::dealloc(ptr, layout)
     }
 
     #[inline]
     unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        moto_runtime::realloc(ptr, layout, new_size)
+        moto_rt::realloc(ptr, layout, new_size)
     }
 }
