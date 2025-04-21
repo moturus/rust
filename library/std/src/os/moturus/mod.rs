3cbad68b6cfeea6ac15436619485ca3cdc3ec296
U. Lasiotus <lasiotus@motor-os.org>
2025-04-20 19:51:30 -0700

    Map E_INVALID_ARGUMENT to ErrorKind::InvalidInput

--- library/std/src/os/moturus/mod.rs
+++ library/std/src/os/moturus/mod.rs
@@ -14,7 +14,7 @@ pub fn map_moturus_error(err: moto_rt::ErrorCode) -> crate::io::Error {
         E_NOT_IMPLEMENTED => ErrorKind::Unsupported,
         E_FILE_TOO_LARGE => ErrorKind::FileTooLarge,
         E_UNEXPECTED_EOF => ErrorKind::UnexpectedEof,
-        E_INVALID_ARGUMENT => ErrorKind::InvalidData,
+        E_INVALID_ARGUMENT => ErrorKind::InvalidInput,
         E_NOT_READY => ErrorKind::WouldBlock,
         E_NOT_CONNECTED => ErrorKind::NotConnected,
         _ => ErrorKind::Other,
